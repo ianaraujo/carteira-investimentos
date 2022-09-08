62fb2905f5a017858f4becb70e19a1931f669a55
ianaraujo <ianaraujo15@gmail.com>
2022-09-08 20:13:46 -0300

    Create view of consolidated current portfolio

--- consolidador_carteira.sql
+++ consolidador_carteira.sql
@@ -1,5 +1,13 @@
-SELECT codigo_negociacao, SUM(quantidade) AS quantidade
+CREATE VIEW vw_carteira_consolidada AS
+
+SELECT codigo_negociacao, 
+
+    SUM(CASE tipo_movimentacao WHEN 'venda' THEN - quantidade ELSE quantidade END) AS quantidade_ajustada,
+
+    ROUND(SUM(quantidade * preco) / SUM(quantidade), 2) AS preco_medio
+
 FROM tb_negociacoes
+
 GROUP BY codigo_negociacao
 
-
+HAVING quantidade_ajustada > 0;
